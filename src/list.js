abfd7c9177d5018058cb2ee5a1ea4f05cc0179b2
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-09-13 18:36:40 +0700

    + list bahasa

---
+++ src/list.js
@@ -0,0 +1,268 @@
+const to_lists = [
+    {
+        code: 'af',
+        name: 'Afrikanns'
+    },
+    {
+        code: 'sq',
+        name: 'Albanian'
+    },
+    {
+        code: 'am',
+        name: 'Amharic'
+    },
+    {
+        code: 'ar',
+        name: 'Arabic'
+    },
+    {
+        code: 'hy',
+        name: 'Armenian'
+    },
+    {
+        code: 'az',
+        name: 'Azerbaijani'
+    },
+    {
+        code: 'eu',
+        name: 'Basque'
+    },
+    {
+        code: 'be',
+        name: 'Belarusian'
+    },
+    {
+        code: 'bn',
+        name: 'Bengali'
+    },
+    {
+        code: 'bs',
+        name: 'Bosnian'
+    },
+    {
+        code: 'bg',
+        name: 'Bulgarian'
+    },
+    {
+        code: 'ca',
+        name: 'Catalan'
+    },
+    {
+        code: 'ceb',
+        name: 'Cebuano'
+    },
+    {
+        code: 'zh',
+        name: 'Chinese'
+    },
+    {
+        code: 'co',
+        name: 'Corsican'
+    },
+    {
+        code: 'hr',
+        name: 'Croatian'
+    },
+    {
+        code: 'cs',
+        name: 'Czech'
+    },
+    {
+        code: 'da',
+        name: 'Danish'
+    },
+    {
+        code: 'nl',
+        name: 'Dutch'
+    },
+    {
+        code: 'en',
+        name: 'English'
+    },
+    {
+        code: 'eo',
+        name: 'Esperanto'
+    },
+    {
+        code: 'et',
+        name: 'Estonian'
+    },
+    {
+        code: 'fi',
+        name: 'Finnish'
+    },
+    {
+        code: 'fr',
+        name: 'French'
+    },
+    {
+        code: 'fy',
+        name: 'Frisian'
+    },
+    {
+        code: 'gl',
+        name: 'Galician'
+    },
+    {
+        code: 'ka',
+        name: 'Georgian'
+    },
+    {
+        code: 'de',
+        name: 'German'
+    },
+    {
+        code: 'el',
+        name: 'Greek'
+    },
+    {
+        code: 'gu',
+        name: 'Gujarati'
+    },
+    {
+        code: 'ht',
+        name: 'Haitian'
+    },
+    {
+        code: 'ha',
+        name: 'Hausa'
+    },
+    {
+        code: 'haw',
+        name: 'Hawaiian'
+    },
+    {
+        code: 'he',
+        name: 'Hebrew'
+    },
+    {
+        code: 'hi',
+        name: 'Hindi'
+    },
+    {
+        code: 'hu',
+        name: 'Hungarian'
+    },
+    {
+        code: 'is',
+        name: 'Icelandic'
+    },
+    {
+        code: 'id',
+        name: 'Indonesian'
+    },
+    {
+        code: 'ga',
+        name: 'Irish'
+    },
+    {
+        code: 'it',
+        name: 'Italian'
+    },
+    {
+        code: 'ja',
+        name: 'Japanese'
+    },
+    {
+        code: 'jv',
+        name: 'Javanese'
+    },
+    {
+        code: 'kk',
+        name: 'Kazakh'
+    },
+    {
+        code: 'ko',
+        name: 'Korean'
+    },
+    {
+        code: 'ku',
+        name: 'Kurdish'
+    },
+    {
+        code: 'lv',
+        name: 'Latvian'
+    },
+    {
+        code: 'lt',
+        name: 'Lituhanian'
+    },
+    {
+        code: 'ms',
+        name: 'Malay'
+    },
+    {
+        code: 'my',
+        name: 'Myanmar'
+    },
+    {
+        code: 'mn',
+        name: 'Mongol'
+    },
+    {
+        code: 'no',
+        name: 'Norwegian'
+    },
+    {
+        code: 'fa',
+        name: 'Persian'
+    },
+    {
+        code: 'pl',
+        name: 'Polish'
+    },
+    {
+        code: 'pt',
+        name: 'Portugis'
+    },
+    {
+        code: 'ro',
+        name: 'Romanian'
+    },
+    {
+        code: 'es',
+        name: 'Spanish'
+    },
+    {
+        code: 'su',
+        name: 'Sundanese'
+    },
+    {
+        code: 'sv',
+        name: 'Swedish'
+    },
+    {
+        code: 'th',
+        name: 'Thai'
+    },
+    {
+        code: 'tr',
+        name: 'Turkish'
+    },
+    {
+        code: 'tk',
+        name: 'Turkmen'
+    },
+    {
+        code: 'uk',
+        name: 'Ukrainian'
+    },
+    {
+        code: 'ur',
+        name: 'Urdu'
+    },
+    {
+        code: 'ug',
+        name: 'Uyghur'
+    },
+    {
+        code: 'vi',
+        name: 'Vietanamese'
+    },
+    {
+        code: 'zu',
+        name: 'Zulu'
+    }
+];
+
+export default to_lists;
