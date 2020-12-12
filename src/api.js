37144b175531e6ed3a2fee345e3d194e3f86c8a4
Azhari Marzan <azhari.marzan@gmail.com>
2020-12-12 12:41:52 +0700

    ^ endpoint api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://api-translate.azharimm.tk/'
+    baseURL: 'https://api-translate.azharimm.tk/translate'
 });
 
 export default api;
