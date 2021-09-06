8436671025f3a2d92cb85118653a7c892af21945
Azhari Muhammad Marzan <azhari.marzan@gmail.com>
2021-09-06 13:57:42 +0700

    update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://api-translate.azharimm.site/'
+    baseURL: 'https://api-translate.azharimm.site/translate'
 });
 
 export default api;
