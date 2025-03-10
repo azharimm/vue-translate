054991734b4697eb1407d0a61d121ae0257e28ae
Azhari Muhammad M <azhari.muhammad@ruangguru.com>
2025-03-11 03:17:41 +0700

    update api endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://api-translate.azharimm.site/translate'
+  baseURL: "https://api-translate.azharimm.web.id/translate",
 });
 
 export default api;
